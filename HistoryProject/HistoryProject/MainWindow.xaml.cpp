32a7a4c5f2777826791e445ec3033bb88a027ee7
Vasil Mladenov <VLMladenov19@codingburgas.bg>
2022-01-29 13:43:09 +0200

    Implement Node insertion

--- HistoryProject/HistoryProject/MainWindow.xaml.cpp
+++ HistoryProject/HistoryProject/MainWindow.xaml.cpp
@@ -27,8 +27,47 @@ namespace winrt::HistoryProject::implementation
         throw hresult_not_implemented();
     }
 
+    int userDay, userMonth, userYear;
+    Node* Head = new Node;
+
     void MainWindow::Button_Add(winrt::Windows::Foundation::IInspectable const& sender, winrt::Microsoft::UI::Xaml::RoutedEventArgs const& e)
     {
-        txtEvent().Text(txtTitle().Text());
+        txtEvent().Text(to_hstring(Head->year));
+    }
+
+    void MainWindow::AssignValues() 
+    {
+        userDay = stoi(to_string(txtDay().Text()));
+        userMonth = stoi(to_string(txtMonth().Text()));
+        userYear = stoi(to_string(txtYear().Text()));
+    }
+
+    void MainWindow::prependNode(Node*& Head)
+    {
+        AssignValues();
+        Node* newNode = new Node{ userDay, userMonth, userYear, NULL, Head };
+        newNode->next = Head;
+        Head->prev = newNode;
+
+        Head = newNode;
+    }
+
+    void MainWindow::appendNode(Node* Head)
+    {
+        AssignValues();
+        Node* newNode = new Node{ userDay, userMonth, userYear };
+
+        Node* temp = new Node;
+        temp = Head;
+        while (temp)
+        {
+            if (temp->next == NULL)
+            {
+                temp->next = newNode;
+                newNode->prev = temp;
+                return;
+            }
+            temp = temp->next;
+        }
     }
 }
--- HistoryProject/HistoryProject/MainWindow.xaml.h
+++ HistoryProject/HistoryProject/MainWindow.xaml.h
@@ -4,6 +4,16 @@
 
 namespace winrt::HistoryProject::implementation
 {
+    struct Node
+    {
+        int day = 0;
+        int month = 0;
+        int year = 0;
+
+        Node* prev = NULL;
+        Node* next = NULL;
+    };
+
     struct MainWindow : MainWindowT<MainWindow>
     {
         MainWindow();
@@ -12,6 +22,9 @@ namespace winrt::HistoryProject::implementation
         void MyProperty(int32_t value);
 
         void Button_Add(winrt::Windows::Foundation::IInspectable const& sender, winrt::Microsoft::UI::Xaml::RoutedEventArgs const& e);
+        void AssignValues();
+        void prependNode(Node*&);
+        void appendNode(Node*);
     };
 }
 
