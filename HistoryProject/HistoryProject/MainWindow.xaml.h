b18ed0e15332a8ef5b4050f08bbb53efd36d1ccf
Vasil Mladenov <VLMladenov19@codingburgas.bg>
2022-01-29 12:51:28 +0200

    Add UI skeleton

---
+++ HistoryProject/HistoryProject/MainWindow.xaml.h
@@ -0,0 +1,23 @@
+﻿#pragma once
+
+#include "MainWindow.g.h"
+
+namespace winrt::HistoryProject::implementation
+{
+    struct MainWindow : MainWindowT<MainWindow>
+    {
+        MainWindow();
+
+        int32_t MyProperty();
+        void MyProperty(int32_t value);
+
+        void myButton_Click(Windows::Foundation::IInspectable const& sender, Microsoft::UI::Xaml::RoutedEventArgs const& args);
+    };
+}
+
+namespace winrt::HistoryProject::factory_implementation
+{
+    struct MainWindow : MainWindowT<MainWindow, implementation::MainWindow>
+    {
+    };
+}
